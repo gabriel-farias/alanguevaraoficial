2346a1486bb21ede3db777236f9eea61a90a806f
Gabriel Farias <gabrielyoung87@gmail.com>
2022-10-16 20:36:20 -0300

    se agrego js y bootstrap

---
+++ app/reveal.js
@@ -0,0 +1,25 @@
+ScrollReveal().reveal(".card", {
+    delay: 450,
+    duration: 2200,
+    reset: true,
+  });
+  ScrollReveal().reveal(".about-me__info", {
+    delay: 650,
+    duration: 2200,
+    reset: true,
+  });
+  ScrollReveal().reveal(".about-me__image", {
+    delay: 1250,
+    duration: 2200,
+    reset: true,
+  });
+  ScrollReveal().reveal(".sponsors", {
+    delay: 450,
+    duration: 2200,
+    reset: true,
+  });
+  ScrollReveal().reveal(".contacto", {
+    delay: 450,
+    duration: 2200,
+    reset: true,
+  });
