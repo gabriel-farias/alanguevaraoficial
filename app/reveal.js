8d23c2992534e249ced649e003c12a20be93bffd
Gabriel Farias <gabrielyoung87@gmail.com>
2022-10-18 07:08:30 -0300

    se agrego slider de sponsors

--- app/reveal.js
+++ app/reveal.js
@@ -1,25 +1,30 @@
+ScrollReveal().reveal(".hero", {
+  delay: 0,
+  duration: 3200,
+  reset: true,
+});
 ScrollReveal().reveal(".card", {
-    delay: 450,
-    duration: 2200,
-    reset: true,
-  });
-  ScrollReveal().reveal(".about-me__info", {
-    delay: 650,
-    duration: 2200,
-    reset: true,
-  });
-  ScrollReveal().reveal(".about-me__image", {
-    delay: 1250,
-    duration: 2200,
-    reset: true,
-  });
-  ScrollReveal().reveal(".sponsors", {
-    delay: 450,
-    duration: 2200,
-    reset: true,
-  });
-  ScrollReveal().reveal(".contacto", {
-    delay: 450,
-    duration: 2200,
-    reset: true,
-  });
+  delay: 450,
+  duration: 2200,
+  reset: true,
+});
+ScrollReveal().reveal(".about-me__info", {
+  delay: 450,
+  duration: 2200,
+  reset: true,
+});
+ScrollReveal().reveal(".about-me__image", {
+  delay: 1250,
+  duration: 2200,
+  reset: true,
+});
+ScrollReveal().reveal(".sponsors", {
+  delay: 450,
+  duration: 2200,
+  reset: true,
+});
+ScrollReveal().reveal(".contacto", {
+  delay: 450,
+  duration: 2200,
+  reset: true,
+});
