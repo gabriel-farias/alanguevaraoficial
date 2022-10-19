6c66622cfc4237c4524e9f053386b5f539d45ef3
Gabriel Farias <gabrielyoung87@gmail.com>
2022-10-19 18:46:27 -0300

    se agrego sponsors y se acomodo un poco el responsive

--- app/reveal.js
+++ app/reveal.js
@@ -2,29 +2,19 @@ ScrollReveal().reveal(".hero", {
   delay: 0,
   duration: 3200,
   reset: true,
+  mobile:false,
 });
-ScrollReveal().reveal(".card", {
+ScrollReveal().reveal(".card,.about-me__info , .sponsors ,.contacto", {
   delay: 450,
   duration: 2200,
   reset: true,
+  mobile:false,
 });
-ScrollReveal().reveal(".about-me__info", {
-  delay: 450,
-  duration: 2200,
-  reset: true,
-});
+
 ScrollReveal().reveal(".about-me__image", {
   delay: 1250,
   duration: 2200,
   reset: true,
+  mobile:false,
 });
-ScrollReveal().reveal(".sponsors", {
-  delay: 450,
-  duration: 2200,
-  reset: true,
-});
-ScrollReveal().reveal(".contacto", {
-  delay: 450,
-  duration: 2200,
-  reset: true,
-});
+
